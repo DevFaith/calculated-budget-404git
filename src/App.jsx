3b3a639a735e5c7b07595c8c1f69994259d2b7b3
DevFaith <waitherafaith@outlook.com>
2024-06-23 23:15:35 +0300

    * feat(App.jsx): add App component with BudgetCalculator component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BudgetCalculator from './Componets/BudgetCalculator';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <BudgetCalculator />
+    </div>
+  );
+}
+
+export default App;
